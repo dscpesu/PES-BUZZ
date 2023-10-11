41fd7126f7451c09299dd29de9fee09594fb3476
Shashank Sundar <shashanksundar16@gmail.com>
2023-10-11 22:40:23 +0530

    Updated splash screen code with a faster animation

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -13,7 +13,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 3), () {
+    Future.delayed(const Duration(milliseconds: 1500), () {
       _navigateToNextScreen();
     });
   }
