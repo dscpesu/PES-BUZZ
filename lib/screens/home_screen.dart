b7dde1d42579a6ef740467ae8a2e097c7b24b1e8
Shashank Sundar <shashanksundar16@gmail.com>
2023-10-24 13:12:31 +0530

    Made changes to app bar adding pes buzz logo and title with user authentication icon as well as date

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '/widgets/display_firestore_data.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,25 +11,107 @@ class _HomeScreenState extends State<HomeScreen> {
   PageController pageController = PageController();
   int currentIndex = 0;
 
+  String _getMonth(int month) {
+    switch (month) {
+      case 1:
+        return 'January';
+      case 2:
+        return 'February';
+      case 3:
+        return 'March';
+      case 4:
+        return 'April';
+      case 5:
+        return 'May';
+      case 6:
+        return 'June';
+      case 7:
+        return 'July';
+      case 8:
+        return 'August';
+      case 9:
+        return 'September';
+      case 10:
+        return 'October';
+      case 11:
+        return 'November';
+      case 12:
+        return 'December';
+      default:
+        return 'Unknown';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('PES Buzz'),
-        backgroundColor: const Color(0xFF4169E1),
-      ),
-      body: PageView(
-        controller: pageController,
-        onPageChanged: (index) {
-          setState(() {
-            currentIndex = index;
-          });
-        },
-        children: <Widget>[
-          DisplayFirestoreData(category: 'Trendy'),
-          DisplayFirestoreData(category: 'Sports'),
-          DisplayFirestoreData(category: 'Department'),
+        backgroundColor: const Color(0xffE2F9EE),
+        leadingWidth: 65,
+        leading: const Padding(
+          padding: EdgeInsets.only(left: 5.0),
+          child: Image(image: AssetImage('assets/logos/PesBuzzLogo.png')),
+        ),
+        title: const Text(
+          'PES Buzz',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 28,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const FaIcon(
+              FontAwesomeIcons.user,
+              color: Colors.black,
+            ),
+          )
         ],
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xffE2F9EE),
+              Color(0xffFBFBD4),
+            ],
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                  top: 10, left: 20, right: 20, bottom: 10),
+              child: Text(
+                '${DateTime.now().day.toString().padLeft(2, '0')}th ${_getMonth(DateTime.now().month)}, ${DateTime.now().year}',
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Expanded(
+              child: PageView(
+                controller: pageController,
+                onPageChanged: (index) {
+                  setState(() {
+                    currentIndex = index;
+                  });
+                },
+                children: <Widget>[
+                  DisplayFirestoreData(category: 'Trendy'),
+                  DisplayFirestoreData(category: 'Sports'),
+                  DisplayFirestoreData(category: 'Department'),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: currentIndex,
